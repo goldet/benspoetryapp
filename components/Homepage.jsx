5ede8fc984d263942b9c649942d63c5c40c8e4c5
DanNisenson <danpablo1@gmail.com>
2023-01-09 15:48:48 +0100

    functional. not styled

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,7 +1,3 @@
 export default function Homepage() {
-    return (
-        <h1 class="text-3xl font-bold underline p-100">
-    Ben Schroeder
-  </h1>
-    )
+  return <h1 className="p-100 text-3xl font-bold underline">Ben Schroeder</h1>;
 }
