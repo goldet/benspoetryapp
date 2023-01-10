f4f3abe299ddf3f7c7ea6e624f701921d5bfe972
Golde Tischler <golde.tischler@gmail.com>
2023-01-10 15:21:35 +0100

    mobile view content and styling

--- components/work/Essays.jsx
+++ components/work/Essays.jsx
@@ -1,8 +1,298 @@
+import Image from "next/image";
+import thewake from "../../public/thewake.jpg";
+
 const WorkEssays = () => {
-    return <div>
-      <h1 className="text-2xl">I HATE CALL ME BY YOUR NAME!!!!! 😠</h1>
-    </div>;
-  };
-  
-  export default WorkEssays;
-  
+  return (
+    <div>
+      <div className="flex flex-col pt-20 md:flex md:flex-col md:items-start lg:flex lg:flex-col lg:items-start">
+        {/* <h1 className="text-2xl underline mb-5 text-left lg:text-left lg:pt-10">Essays</h1> */}
+        <h1 className="px-5 pb-5 text-left text-2xl lg:pt-10 lg:text-left">
+          A Theater of the Impossible: The Problem with{" "}
+          <em>Call Me by Your Name</em>
+        </h1>
+
+        <div className="w-40 pl-5 lg:flex lg:flex-row">
+          <Image
+            className=""
+            src={thewake}
+            alt="logo of The Wake, the magazine that originally published this essay"
+          ></Image>
+        </div>
+
+        <h4 className="py-5 pl-5 pr-16 text-sm">
+          (Originally published online in <em>The Wake</em> on February 12,
+          2018)
+        </h4>
+        <p className="pl-5 pr-10 pb-5">
+          Our story begins not in a colorful town in northern Italy, but in a
+          drab one in southeastern Wisconsin. In a community that had once all
+          but passed a law banning the mowing of lawns on Sundays, I used to
+          hide in my bed with a weathered MacBook and watch all the gay movies I
+          could find for free on the internet. Each had its own cinematic
+          nuance, but a common thread united them nonetheless: from{" "}
+          <em>Speedway Junky to Head On</em>, all ended with the failure,
+          collapse, and lonely heartbreak of any queer relationship depicted.
+          Whether friendly or romantic, the relationships I saw taught me that I
+          would never be happy, I would never have a healthy social life, and I
+          would always be alone, distracting myself from this misery by pursuing
+          increasingly promiscuous interactions. And herein lies the first
+          problem with <em>Call Me by Your Name</em>.
+        </p>
+        <p className="pl-5 pr-10 pb-5">
+          Although the setting and cinematography are more gorgeous than its
+          genetic predecessors, <em>Call Me by Your Name</em> is still just the
+          same hetero-cinematographic viewpoint on homosexual and queer
+          relationships with a facelift. Sure, the movie-going public has become
+          far more comfortable with the idea of two men kissing on screen than
+          they were in, say, 2006 when <em>Brokeback Mountain</em> appeared in
+          movie theaters, but not comfortable enough not to pan away from
+          anything <em>beyond</em> a kiss shared between men, which happens in{" "}
+          <em>Call Me by Your Name</em>. When Elio and Oliver go to bed together
+          for the first time, the camera swings toward a tree just outside the
+          window. However, we see a bird’s-eye view of the conjugal heterosexual
+          relationship between Elio and Marzia, seventeen-year-olds having what
+          could otherwise be called a cliché, teen-movie, summer-fling, sexual
+          awakening. No matter how you interpret the filmic merits of an
+          arborescent effacement of homosexual intimacy, there is a clear gap
+          between how heterosexual and homosexual relationships are depicted
+          early on.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          But the lack of gay sex in a movie meant to be about a gay
+          relationship is not the biggest problem with{" "}
+          <em>Call Me by Your Name</em>; anyone interested in seeing gay sex in
+          its entirety can find it elsewhere, and likely for free, on the
+          internet. Still, Luca Guadagnino has an argument for his exclusion of
+          the coital scene between Elio and Oliver: “I refuse with strong
+          firmness that I was coy in not showing [their sex scene], because I
+          think that Oliver and Elio…displayed a very strong intimacy and
+          closeness in so many ways and it was enough (Guadagnino, quoted by
+          Dry). It is disheartening that Guadagnino speaks about gay sex as
+          though it were somehow outside of the closeness and intimacy he
+          describes. In fact, the fulfilment of desire through sex deserves
+          better than to be portrayed as a mere physical act that debases the
+          emotional connection between Elio and Oliver. Attempting to “purify”
+          the queer relationship by effacing its physical element is othering in
+          a subtly harmful way. Elio and Marzia also share an intimacy and
+          closeness that is still allowed its physical aspect, implying
+          heterosexual relationships are inherently pure.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          Though by itself the lack of a visually explicit sex scene is
+          immaterial in the film’s merit as queer art, juxtaposed with an
+          in-your-face heterosexuality the difference becomes the problematic
+          element. What isn’t there becomes the focus, and ultimately, the
+          viewer is distracted by the implicit—that a gay relationship should be
+          secret, invisible, unspoken—and pushed out of the film’s built world.
+          What we end up with is a theater of the impossible centered on a
+          foregone conclusion, and a broken one at that. What we lose is a
+          valuable meditation on desire as it is eclipsed by the specter of
+          cliché.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          And that is the tragedy of <em>Call Me by Your Name’s</em> failure as
+          queer art despite depicting a gay relationship. It espouses the same
+          view of queer relationships as inevitably flawed in light of the
+          ideal, heterosexual relationships flourishing around them—a
+          fundamentally clichéd perspective. Clichés teach us nothing new about
+          how to look at the world, which is exactly what, if you see no other
+          value in us, we marginalized peoples can bring to art: new
+          perspectives.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          I understand that straight artists have often misunderstood and
+          misrepresented us as being sex obsessed—single faceted characters
+          interested only in the physical—but that is precisely why Guadagnino
+          should have included the sex scene. By his own logic, the sex would
+          have been intimate and close, a perspective on gay sex unfamiliar to
+          straight audiences. While <em>Head On’s</em> ending of a queer man
+          deciding quite explicitly to live for casual sex alone is an imperfect
+          and problematic one, so too is <em>Speedway Junky’s</em>, which
+          despite focusing on a queer friendship, still ends in a dramatization
+          of unattainability that cinema at this point had decided was inherent
+          to queer lives. As such, when <em>Call Me by Your Name</em> ends with
+          Elio crying alone after learning Oliver will marry a woman, viewers
+          are shown the same cliché that was already tired by the release of{" "}
+          <em>Speedway Junky</em>. Here too, queer relationships are different
+          because they are unsustainable, unhealthy, and self-destructive.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          But that is not our story, and if we want a truly queer cinema, we
+          must demand better. We must demand nuanced queer stories where when
+          societal pressures play a strong role, as in our lives, they do not
+          determine the possibility of queer relationships. Queer desire should
+          be put carefully and lovingly on display, probed and interrogated
+          until it gives us something beautiful and new.{" "}
+          <em>In Call Me by Your</em>
+          Name, desire again tried to show us what it could teach, but its
+          message got lost in the comfortable rhythm of telling an old story the
+          same way.
+        </p>
+
+        <p className="pl-5 pr-10 pb-2 text-center">Works Cited</p>
+        <p className="pl-5 pr-10 pb-5 text-xs">
+          Dry, Jude. “Luca Guadagnino Had a Good Reason for Not Showing the Sex
+          Scene in ‘Call Me by Your Name.’” <em>Indiewire</em>, 22 Dec. 2017,{" "}
+          <u>https://owl.english.purdue.edu/owl/resource/747/08/</u>. Accessed
+          22 Jan. 2018.
+        </p>
+
+        <h1 className="px-5 pt-10 pb-1 text-left text-2xl lg:pt-10 lg:text-left">
+          The Variable of Absence: Caitlin Bailey’s <em>Solve</em> for Desire{" "}
+        </h1>
+
+        <h4 className="py-5 pl-5 pr-16 text-sm">
+          (Originally published in Issue 65 of <em>Great River Review</em> pages
+          133-135)
+        </h4>
+
+        <p className="pl-5 pr-10 pb-5">
+          In the very first poem of her debut collection{" "}
+          <em>Solve for Desire</em>, Caitlin Bailey begins to carve out the
+          “hollow” where “only the sweetest grass” grows. “Whoever Drinks from
+          Me” immediately presents “Brother as thirst,” a connection suggesting
+          not only an absence of the brother, who may be understood as Georg
+          Trakl, the great Modernist poet, but also a painful absence. Yet, the
+          speaker of the poem, possibly the poet’s sister, Grete Trakl, goes on
+          to describe “Water [as] meant to wound,” compli- cating the pain of
+          the brother’s absence, making the case that relief from thirst would
+          be no relief at all, just a different kind of pain, a wound.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          But what of the sweetness of the grass? Perhaps that image of growth,
+          and its qualification—surrounded by pain—nods to the complicated re-
+          lationship between the poet Georg Trakl and his sister Grete Trakl,
+          the excavation of whom was Bailey’s inciting task. In the process of
+          tracing Grete’s life, peppered with her skill at the piano and
+          addiction to drugs, Bailey’s imagination comes to life. Through her
+          careful study of Georg’s poetry and the letters shared by the
+          siblings, Grete’s voice and others Bailey uncovered along the way ring
+          through with a clarity all the more haunting for their having burst
+          into an empty room. The project of <em>Solve for Desire</em> takes on
+          another task, then: filling the longstanding absence of those voices
+          and laying bare that which refuses to go unnoticed.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          And it is from this framework of pain without relief, of shifting and
+          variable absence that “Whoever Drinks from Me” establishes that we
+          begin to understand the reason Grete tells Georg “I will never, never
+          leave you.” In fact, the replacement of desire and grief by other
+          desire and other grief is a thread woven throughout{" "}
+          <em>Solve for Desire</em>. “Grete Writes to Georg at War” puts it best
+          when Grete says “G, you might have been the prize. / The salt packed
+          in my organs, / the magnificent shore.” Here, she imagines having the
+          object of her desire, replacing the variable of absence with the
+          specification of presence, but that which should fill a gap—the salt
+          between the organs, the shore between the 134 Great River Review land
+          and sea—actually becomes the gap, embodying the space between the
+          organs, between the land the sea.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          Again, in “Some Elaborate” Grete laments, “You leave me every day in
+          small ways, / and I imagine your organs must be eroding, / your skin
+          the only shoreline I want to visit.” Her desire becomes wrapped up in
+          this replacement of the gap, this undefinable variable of absence,
+          making her desire at once more tragic and nuanced. It is not Georg’s
+          skin she wants to visit but the space of absence; Grete wants to
+          embody for herself the condition that seems to predicate her desire
+          for Georg:
+        </p>
+
+        <p className="px-14 pb-5">
+          I am hostage to your absence, <br />
+          filling a bucket, even the most familiar
+          <br />
+          street a wilderness I don’t recognize, <br />a waterline I keep
+          expecting will change
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          This final stanza ends without punctuation in hopes that the change is
+          yet to come, but as before, the change is simply a shift from one
+          absence to another, a change that changes nothing, and the lack of a
+          period, too, does not make this fact any less final or inescapable.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          Just before section three, however, there is a shift from desire to
+          grief, and it is there, in “Somewhere a Key,” that the permeability of
+          the two emotions, their interconnectedness, becomes clearer:
+        </p>
+
+        <p className="px-14 pb-5">
+          You are a sickness still lingering, rotten pit to be rent from the
+          chest, sour blood to drain. Crows repeatedly fly into the windows.
+          Done numbing myself, or else the money’s run out. You must know I keep
+          writing these letters, missives dropped around the city, crumbs for
+          your ghost. Almost all the rooms inside me are closed. Each time I
+          write your name a key somewhere turns in a lock.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          Desire lingers, but it has rotted, fermented into grief, for now that
+          the money’s run out—now that the very sign of variable absence is used
+          up—and now that Georg is a ghost, desire is pure absence, an unful-
+          fillable lack looping back on itself. Crows throw themselves against
+          the invisible barriers separating inside from outside, revealing them
+          “the way glass catches the light” (“On Never Marrying Him”), likewise
+          revealing absence itself as a kind of presence. Desire and grief are
+          the refraction of absence, which is at once light and glass—the
+          presence of absence revealed by absence of the desired.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          In fact, in “Grete Asks the Hard Question,” in section three, Grete’s
+          desire to experience the space of absence, to embody the cause of her
+          desire in grief—to take them for herself and become the determiner of
+          her own emotion—becomes clear when she says “I tell you my body is a
+          hollow, a fence, how there are so many / colors I’ll never see. Let me
+          be refracted as you see best.” And this is Bailey’s triumph in{" "}
+          <em>Solve for Desire</em>: not only does she unite desire and grief in
+          an indeterminate space all too familiar to those who have lived it but
+          she lays bare the desire within that space to master it, to overcome
+          and own the monstrous desire-grief hybrid created by the death of a
+          loved one. But, perhaps devastatingly so, the unattainability of this
+          desire emerges too; just as Grete’s desire to replace the absences—the
+          gaps—simply produces new absences, when thinking of deceased loved
+          ones, when “Sometimes [they] visit [us] / [their] hands become birds,
+          unfurl the whole forest.” Grief opens up, ex- pands and explains the
+          forest, but in so doing moves further out of reach.
+        </p>
+
+        <p className="pl-5 pr-10 pb-5">
+          In <em>Solve for Desire</em>, Caitlin Bailey offers the gift of a voice
+          excavated from the history of her brother’s fame and tragedy. A task
+          admirable in and of itself, Bailey empowers this voice so thoroughly
+          that it begins to call for emancipation from its own grief and desire.
+          Grete speaks so clear- ly through <em>Solve for Desire</em> that she begins to
+          excavate herself, brushing off what debris is left behind by the work
+          Bailey has already done to free her. What’s more is the way this
+          excavation, this self-emancipation, is interwoven with that very same
+          grief and desire that seem to conceal the voice at times. Through
+          intricate turns and returns to the examination of separation and
+          absence, Bailey and Grete work together to reveal grief and desire in
+          all their complexity at the same time as they complicate prevailing
+          notions of what grief and desire can do. <em>Solve for Desire</em> makes the
+          productive potential of wrestling with complex, and at times insur-
+          mountable or ungraspable emotions, clear; the struggle to understand,
+          embody, and overcome this desire-grief is the condition of possibility
+          for freedom from the very same. Grief and desire become the tools
+          through which we work toward control, through which we see the “light
+          stream- ing through glass, / streaked onto palms.” By exposing
+          absence’s tenu- ous hold on desire and grief, Bailey shows us that the
+          tools are, in fact, in our hands.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WorkEssays;
