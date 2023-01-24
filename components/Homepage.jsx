442d6ae68b83f18b27a608020f3bf2ee9ab20c17
DanNisenson <danpablo1@gmail.com>
2023-01-24 13:42:43 +0100

    try brutalist title in home page. maybe scroll to bio?

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -6,20 +6,20 @@ export default function Homepage() {
     <>
       <div className="pt-8 flex flex-col md:items-start md:px-[20vw] lg:px-0 lg:flex-row lg:justify-between">
         <div className="lg:flex lg:flex-col">
-          <h1 className="flex flex-col items-center ml-[-1vw] text-8xl  tracking-tighter title-line-height md:text-9xl lg:pt-8">
+          <h1 className="flex flex-col items-center ml-[-1vw] text-8xl leading-[0.6] tracking-[-8px] md:tracking-[-20px] md:leading-[0.6] md:text-[250px] lg:pt-8">
             Ben <br /> Schroeder
           </h1>
-          <h2 className="text-5xl grey-text pt-4">Poet</h2>
+          <h2 className="text-5xl grey-text pt-4 md:pt-8">Little Poet</h2>
         </div>
 
         <div className="lg:flex lg:flex-row pt-8">
-          <Image
+          {/* <Image
             className="rounded-md"
             src={benphoto2}
             alt="image of Ben Schroeder"
             width="300px"
             height="300px"
-          />
+          /> */}
         </div>
       </div>
     </>
