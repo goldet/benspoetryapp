0fd3bfdc5af8aafff42f776835d5319640ac160c
Golde Tischler <golde.tischler@gmail.com>
2023-02-24 15:00:18 +0100

    Issues with footer position

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { GoMarkGithub } from "react-icons/go";
 
 const Footer = () => {
   return (
-     <footer className="bottom-0">
+     <footer className="fixed bottom-0">
       <div className="m-auto w-full">
         <div className="flex gap-2 justify-stretch">
           2023 Created By <GoMarkGithub className="flex" />{" "}
@@ -19,3 +19,15 @@ const Footer = () => {
 };
 
 export default Footer;
+
+       {/*  linkedin option */}
+       /* import { AiFillLinkedin } from "react-icons/ai";
+<div className="flex gap-2 justify-stretch">
+2023 Created By <AiFillLinkedin className="flex " />{" "}
+<a href="https://www.linkedin.com/in/golde-tischler/" target="blank">
+  Golde Tischler
+</a> and 
+<a href="https://www.linkedin.com/in/dan-nisenson/" target="blank">
+  Dan Nisenson
+</a>
+</div> */
