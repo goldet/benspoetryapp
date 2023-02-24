7abda052e3ca1ae7c534b9a4b52f8adc51859502
Golde Tischler <golde.tischler@gmail.com>
2023-02-24 14:57:36 +0100

    linked in footer options

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,20 +1,35 @@
 import { GoMarkGithub } from "react-icons/go";
+import { AiFillLinkedin } from "react-icons/ai";
 
 const Footer = () => {
   return (
-     <footer className="bottom-0">
+    <footer className="bottom-0">
       <div className="m-auto w-full">
-        <div className="flex gap-2 justify-stretch">
+        <div className="justify-stretch flex gap-2">
           2023 Created By <GoMarkGithub className="flex" />{" "}
           <a href="https://github.com/goldet" target="blank">
             Golde Tischler
-          </a> and 
+          </a>{" "}
+          and
           <a href="https://github.com/DanNisenson" target="blank">
             Dan Nisenson
           </a>
         </div>
-     </div>
-     </footer> 
+
+        {/*  linkedin option */}
+
+        {/* <div className="justify-stretch flex gap-2">
+          2023 Created By <AiFillLinkedin className="flex " />{" "}
+          <a href="https://www.linkedin.com/in/golde-tischler/" target="blank">
+            Golde Tischler
+          </a>{" "}
+          and
+          <a href="https://www.linkedin.com/in/dan-nisenson/" target="blank">
+            Dan Nisenson
+          </a>
+        </div> */}
+      </div>
+    </footer>
   );
 };
 
